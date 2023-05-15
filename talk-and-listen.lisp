3b1e56824446af973b1be8c3f14a83eced4c22a8
Gleefre <varedif.a.s@gmail.com>
2023-05-15 17:19:10 +0200

    Add R button.

--- talk-and-listen.lisp
+++ talk-and-listen.lisp
@@ -1,6 +1,7 @@
 ;; Running:
 ;;   sbcl --load talk-and-listen.lisp --quit
 ;; Hold space to record.
+;; Press R to record till the end of the frame.
 
 (ql:quickload '(:sketch :cl-mixed :cl-mixed-pulse))
 
@@ -11,8 +12,7 @@
 
 (sketch:defsketch talk-and-listen ((sketch:height 300)
                                    (sketch:width 400)
-                                   (repeat-segment NIL)
-                                   (output-pack NIL))
+                                   (repeat-segment NIL))
   (sketch:with-font (sketch:make-font :align :center)
     (sketch:text (format NIL "~a: ~a mode, ~$ sec"
                          repeat-segment
@@ -22,11 +22,15 @@
                  (/ sketch:height 3))))
 
 (defmethod kit.sdl2:keyboard-event ((app talk-and-listen) state ts rep? keysym)
-  (when (and (not rep?) (eq :scancode-space (sdl2:scancode keysym)))
-    (if (eq :keydown state)
-        (setf (mixed:volume (talk-and-listen-output-pack app)) 0
-              *mode* :record)
-        (setf *mode* :play))))
+  (when (not rep?)
+    (case (sdl2:scancode keysym)
+      (:scancode-space
+       (if (eq :keydown state)
+           (setf *mode* :record)
+           (setf *mode* :play)))
+      (:scancode-r
+       (when (eq :keydown state)
+         (setf *mode* :record-once-set))))))
 
 (defmethod kit.sdl2:close-window :after ((app talk-and-listen))
   (setf *run* NIL))
@@ -35,6 +39,8 @@
   (mixed:with-objects ((input (mixed:make-unpacker))
                        (output (mixed:make-packer))
                        (rep-r (mixed:make-repeat :time time :mode *mode*))
+                       (fr (mixed:make-biquad-filter))
+                       (fl (mixed:make-biquad-filter))
                        (rep-l (mixed:make-repeat :time time :mode *mode*))
                        (drain (make-instance 'org.shirakumo.fraf.mixed.pulse:drain
                                              :pack output))
@@ -42,20 +48,30 @@
                                            :pack input)))
     (make-instance 'talk-and-listen
                    :resizable T
-                   :repeat-segment rep-r
-                   :output-pack output)
-    (mixed:with-buffers 500 (l r ll rr)
+                   :repeat-segment rep-r)
+    (mixed:with-buffers 500 (l r ll rr fll frr)
       (mixed:connect input :left rep-l :mono l)
-      (mixed:connect rep-l :mono output :left ll)
+      (mixed:connect rep-l :mono fl :mono fll)
+      (mixed:connect fl :mono output :left ll)
       (mixed:connect input :right rep-r :mono r)
-      (mixed:connect rep-r :mono output :right rr)
-      (mixed:with-chain chain (mic input rep-l rep-r output drain)
+      (mixed:connect rep-r :mono fr :mono frr)
+      (mixed:connect fr :mono output :right rr)
+      (mixed:with-chain chain (mic input rep-l fl rep-r fr output drain)
         (loop while *run*
               do (unless (eq *mode* (mixed:repeat-mode rep-r))
-                   (setf (mixed:repeat-mode rep-r) *mode*
-                         (mixed:repeat-mode rep-l) *mode*)
-                   (when (eq *mode* :play)
-                     (setf (mixed:volume output) 1)))
+                   (case *mode*
+                     (:record-once-set
+                      (setf *mode* :record-once
+                            (mixed:repeat-mode rep-r) :record-once
+                            (mixed:repeat-mode rep-l) :record-once))
+                     (:record-once
+                      (setf *mode* (mixed:repeat-mode rep-r)))
+                     ((:record :play)
+                      (setf (mixed:repeat-mode rep-r) *mode*
+                            (mixed:repeat-mode rep-l) *mode*)))
+                   (if (eq *mode* :play)
+                       (setf (mixed:volume output) 1)
+                       (setf (mixed:volume output) 0)))
                  (mixed:mix chain))))))
 
 (start)
