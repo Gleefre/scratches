1b8a6229f0952c162d43960a55071178035494d8
Gleefre <varedif.a.s@gmail.com>
2023-07-02 20:54:07 +0200

    More consistent declarations

--- scratches/fibonacci-with-registers-and-stack.lisp
+++ scratches/fibonacci-with-registers-and-stack.lisp
@@ -1,12 +1,12 @@
 (defun fib-iter (n)
   (declare (optimize speed (safety 0) (debug 0))
-           (type (unsigned-byte 32) n))
-  (let ((stack (make-array (* 2 n) :element-type '(unsigned-byte 32)))
+           (type fixnum n))
+  (let ((stack (make-array (* 2 n) :element-type 'fixnum))
         (stack-pointer 0))
-    (declare (type (unsigned-byte 32) stack-pointer))
+    (declare (type fixnum stack-pointer))
     ;; stack
     (let ((x n) (res 0) (continue 0))
-      (declare (type (unsigned-byte 32) res x continue))
+      (declare (type fixnum res x continue))
       ;; registers
       (macrolet ((save (reg)
                    `(setf (aref stack stack-pointer) ,reg
@@ -28,13 +28,13 @@
              ;; prepare call to (fib (- n 1))
              (save continue)
              (save x)
-             (setf x (- x 1))
+             (setf x (the fixnum (- x 1)))
              (setf continue 1)
              (go fib-rec)
            after-fib-x-1
              ;; prepare call to (fib (- n 2))
              (restore x)
-             (setf x (- x 2))
+             (setf x (the fixnum (- x 2)))
              (save res)
              (setf continue 2)
              (go fib-rec)
@@ -42,7 +42,7 @@
              ;; add results together and return
              (setf x res)
              (restore res)
-             (setf res (+ res x))
+             (setf res (the fixnum (+ res x)))
              (restore continue)
              (continue-dispatch)
            done
@@ -53,8 +53,8 @@
            (type fixnum n))
   (if (< n 2)
       n
-      (the fixnum (+ (the fixnum (fib (- n 1)))
-                     (the fixnum (fib (- n 2)))))))
+      (the fixnum (+ (the fixnum (fib (the fixnum (- n 1))))
+                     (the fixnum (fib (the fixnum (- n 2))))))))
 
 #|
 ;;;; SBCL 2.3.6
